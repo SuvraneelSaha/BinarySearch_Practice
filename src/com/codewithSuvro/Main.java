f9ff5b36e0ea72d2b1b0be429f5f6b357137d007
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-07 23:52:15 +0530

    created new repo named Binary search Practice

---
+++ src/com/codewithSuvro/Main.java
@@ -0,0 +1,9 @@
+package com.codewithSuvro;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Binary search Practice :");
+	// write your code here
+    }
+}
