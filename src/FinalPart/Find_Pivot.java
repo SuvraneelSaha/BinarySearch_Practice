b215473e0dbfacc06b0f5dcc3484cc07396551ea
Suvraneel _Saha <81958235+SuvraneelSaha@users.noreply.github.com>
2022-03-14 01:08:22 +0530

    PIVOT ☮

--- src/FinalPart/Find_Pivot.java
+++ src/FinalPart/Find_Pivot.java
@@ -7,9 +7,31 @@ public class Find_Pivot {
         // 0 , 1 , 2 is also in ascending order
         // so the omly condition in 7 , 0
         // is in descending  order
-        // condition -- mid > mid + 1 --. that is the pivot element  now
+        // condition -- mid > mid + 1 --. mid is the pivot  that is the pivot element  now
         // Friends pivot lol
         // (mid) > mid + 1  -- > this is case 1
         //  2nd case ANS  -- > if (mid) < mid - 1 elemement then  the ans will be mid - 1 element or index
+        // 3rd CASE
+        // start element >= mid element
+        // [ 4,5,6,3,2,1,0]
+        // if the mid element is lesser than the start element then
+        // the elements on the right hand side of the middle element < the start element
+        // in the case all the all the elements from mid will be smaller than the start element < start
+        //  hence we can ignore all the elements since we are looking for the peak element ie the largest element
+        // then the end = mid - 1 ;
+        // CASE 4 --> Start element < the mid element
+        // ex - {3,4,5,6,2}
+        // the start element < the mid element
+        // so the greatest element or the peak element will be on the right side of the mid element
+        // so start = mid + 1 ;
+        // if the middle element was the pivot thenit would have been returned in the case 1 and case 2
+        // hence bigger nos lie ahead ;  hence ignore mid and put start = mid + 1 ;
+
+
+        // PS :
+        // This wont work for duplicate arrays
+
+
+
     }
 }
---
+++ src/FinalPart/Rotated_Binary_Search.java
@@ -0,0 +1,43 @@
+package FinalPart;
+// Question link : https://leetcode.com/problems/search-in-rotated-sorted-array/
+public class Rotated_Binary_Search {
+    public static void main(String[] args) {
+        // Crucial steps :
+        // 1 ] find the pivot in the array
+
+    }
+    static int Find_Pivot(int[] arr)
+    {
+        int start = 0 ;
+        int end = arr.length-1;
+        while (start<=end)
+        {
+            int mid = start + (end - start)/2;// cause of ramge
+            // 4 cases now
+            // CASE 1
+            if(mid < end && arr[mid] > arr[mid+1]) // && operator both true then true if both false then false
+                // or else it will not run
+            {
+                return mid;
+            }
+            // CASE 2
+            if(mid > start && arr[mid]< arr[mid-1])
+            {
+                return mid - 1 ;
+            }
+            //CASE 3
+            if(arr[mid] <= arr[start])
+            {
+                end = mid - 1 ;
+            }
+            //CASE 4 ie arr[mid] >= arr[start]
+            else {
+                start = mid + 1; // cause the greater elements or larger element will lie on the right side of the middle element
+            }
+
+
+        }
+        return -1; 
+    }
+
+}
